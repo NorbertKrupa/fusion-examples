9b643f78058068a0012672803ee682ed1ef08e3a
Will Tseng <wietseng@gmail.com>
2018-06-13 15:45:22 -0500

    Update RHEL_7_Fusion_Setup.sh
    Updated JDK Url

--- misc/scripts/RHEL_7_Fusion_Setup.sh
+++ misc/scripts/RHEL_7_Fusion_Setup.sh
@@ -20,7 +20,8 @@ sudo yum -y install vim
 # Use the RPM method....
 #
 #JDK_URL="http://download.oracle.com/otn-pub/java/jdk/8u151-b12/e758a0de34e24606bca991d704f6dcbf/jdk-8u151-linux-x64.rpm"
-JDK_URL="http://download.oracle.com/otn-pub/java/jdk/8u161-b12/2f38c3b165be4555a1fa6e98c45e0808/jdk-8u161-linux-x64.rpm"
+#JDK_URL="http://download.oracle.com/otn-pub/java/jdk/8u161-b12/2f38c3b165be4555a1fa6e98c45e0808/jdk-8u161-linux-x64.rpm"
+JDK_URL="http://download.oracle.com/otn-pub/java/jdk/8u171-b11/512cd62ec5174c3487ac17c61aaa89e8/jdk-8u171-linux-x64.rpm"
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "$JDK_URL" -O "jdk.rpm"
 if [ ! -s $filename ]; then
   echo "Could not download java, you may need to setup http_proxy and https_proxy environment variables."
